50194a81a39a4483755c6e524d612c0a18380c09
izazrizqullah <izazrizqullah@gmail.com>
2024-12-09 13:20:04 +0700

    update: all recipes

--- src/allrecipes/allrecipes.service.ts
+++ src/allrecipes/allrecipes.service.ts
@@ -22,15 +22,14 @@ export class AllrecipesService {
         page.click('.mntl-search-form__button'),
       ]);
       return await page.$$eval(
-        '#mntl-search-results_1-0 #mntl-search-results__content_1-0',
+        '.mntl-search-results .mntl-universal-card-list--extendable',
         (resultItems) =>
           resultItems.map((resultItem) => {
             const url = resultItem.querySelector('a').href;
-            const title = resultItem.querySelector(
-              '.card__content .card__title-text',
-            )?.textContent;
+            const title =
+              resultItem.querySelector('.card__title-text')?.textContent;
             const ratings = resultItem.querySelector(
-              '#mntl-recipe-card-meta_1-0 .mm-recipes-card-meta__rating-count-number',
+              '.mm-recipes-card-meta__rating-count-number',
             )?.textContent;
             return { url, title, ratings };
           }),
